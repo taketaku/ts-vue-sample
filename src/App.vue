b7fb20dee16f880d40cafaf4caabb5d690f79f07
Takuya Takeda <vbvbsp48@gmail.com>
2019-05-20 18:04:41 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+  #app
+    img(alt='Vue logo', src='./assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js + TypeScript App')
 </template>
 
 <script lang="ts">
